cb62477482a5e1790243facc0f05509b3e13fa48
phj1120 <hyeonj1998@naver.com>
2023-01-21 23:32:13 +0900

    feat(all): 프로젝트 기본 설정

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar flat>
+    <v-btn @click="handleClickList">목록</v-btn>
+    <v-btn @click="handleClickSample">샘플테스트</v-btn>
+    <v-btn @click="handleClickLogin">로그인</v-btn>
+    <v-btn @click="handleClickLogout">로그아웃</v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+import {ref} from "vue";
+import useMemberInfo from "@/store/useMemberInfo";
+import {useRouter} from "vue-router";
+
+const memberStore = useMemberInfo();
+const router = useRouter();
+const handleClickList = () =>{
+  router.push({name:'TodoListPage'})
+}
+
+const handleClickSample = () =>{
+  router.push({name:'SampleTest'})
+}
+
+const handleClickLogin = () =>{
+  router.push({name:'LoginPage'})
+}
+
+const handleClickLogout = ()=>{
+  memberStore.logout()
+}
+
+</script>

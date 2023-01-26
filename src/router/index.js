5f93999cd94ce706dfc3b0f2d749deeaeb2c26e6
twoswim <twoswim@github.com>
2023-01-26 15:09:37 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -45,6 +45,28 @@ const routes = [
     name: 'LogoutPage',
     redirect: '/',
   },
+  {
+    path: '/admin',
+    name: 'AdminPage',
+    component: () => import('@/views/AdminPage.vue'),
+    children: [
+      {
+        path: '/admin/rollingList',
+        name: 'AdminRollingList',
+        component: () => import('@/components/admin/rolling/AdminRollingListComponent.vue')
+      },
+      {
+        path: '/admin/rollingDetail',
+        name: 'AdminRollingDetail',
+        component: () => import('@/components/admin/rolling/AdminRollingDetailComponent.vue')
+      },
+      {
+        path: '/admin/memberList',
+        name: 'AdminMemberList',
+        component: () => import('@/components/admin/member/MemberListComponent.vue')
+      },
+    ]
+  },
 ]
 
 const router = createRouter({

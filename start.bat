16d9e13df54a41c0c0ed3f2931e2a13ef925bfbc
Seunghyeon Jung <armyso9506@plateer.com>
2023-01-27 10:24:11 +0900

    Jenkins Script 수정

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-taskkill /f /im node.exe > NUL
+for /f "tokens=5" %%p in (' netstat -ano ^| find "LISTENING" ^| find "3000" ') do taskkill /F /PID %%p > NUL
 IF NOT ERRORLEVEL 1 (
 	ECHO NOT START PROJECT.
 	yarn dev

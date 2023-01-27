5cdc0291b079a53fe1c2b61bca5ecb980aee0d14
Seunghyeon Jung <armyso9506@plateer.com>
2023-01-27 10:09:40 +0900

    Jenkins 스크립트 수정

--- start.bat
+++ start.bat
@@ -1,12 +1,14 @@
 taskkill /f /im node.exe > NUL
 IF NOT ERRORLEVEL 1 (
 	ECHO NOT START PROJECT.
+	yarn dev
 ) ELSE (
-	ECHO PROJECT KILL OK.	
+	ECHO PROJECT KILL OK.
+	start yarn dev
 )
 
 :END
 
-start yarn dev
+
 
 Exit 0

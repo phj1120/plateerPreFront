5ddef7985ba29ad486cbf3b9758cc5724983baba
Seunghyeon Jung <armyso9506@plateer.com>
2023-02-06 17:47:45 +0900

    신규 TOY

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,5 +1,4 @@
 <template>
-<!--
   <v-app-bar flat>
     <v-app-bar-title>
       <v-icon icon="mdi-circle-slice-6" />
@@ -7,7 +6,6 @@
       Essentials Preset
     </v-app-bar-title>
   </v-app-bar>
--->
 </template>
 
 <script setup>
